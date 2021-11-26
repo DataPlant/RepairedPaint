8afe78a18ad9261eb17043fe0a3a0579ab79502e
DataPlant <dataplant@outlook.com>
2021-11-26 18:18:09 -0500

    testing2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 // const io = require('socket.io')
 
 const app = express();
-app.use('./app', express.static('./app'))
+app.use(express.static('app'))
 
 
 app.listen(process.env.PORT || '0.0.0.0', () => {
